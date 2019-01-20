03a95c5f4e0fd84beee2db0e8a80c9a160725dc8
samurrai <samurrrai.r@gmail.com>
2019-01-21 00:47:22 +0600

    Домашняя работа по классам

---
+++ Car.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassHomework
+{
+    public partial class Car
+    {
+        private static string _company;
+        private static bool _isFourWheeled;
+
+        private int _speed;
+        private string _model;
+        private string _color;
+        private bool _isPassengerCar;
+
+        static Car()
+        {
+            _company = "Mitsubishi";
+            _isFourWheeled = true;
+        }
+
+        public Car()
+        {
+            _company = "Mitsubishi";
+            _isFourWheeled = true;
+        }
+
+        public Car(int speed)
+        {
+            _speed = speed;
+            _company = "Mitsubishi";
+            _isFourWheeled = true;
+        }
+
+        public Car(bool isPassengerCar) {
+            _isPassengerCar = isPassengerCar;
+            _company = "Mitsubishi";
+            _isFourWheeled = true;
+        }
+
+        public Car(string model, string color)
+        {
+            _model = model;
+            _color = color;
+            _company = "Mitsubishi";
+            _isFourWheeled = true;
+        }
+
+        public Car(ref Car car)
+        {
+            _speed = car._speed;
+            _model = car._model;
+            _color = car._color;
+            _isPassengerCar = car._isPassengerCar;
+        }
+
+        public int GetSpeed()
+        {
+            return _speed;
+        }
+        public string GetModel()
+        {
+            return _model;
+        }
+        public string GetColor()
+        {
+            return _color;
+        }
+        public bool IsPassengerCar()
+        {
+            return _isPassengerCar;
+        }
+
+        public void SetSpeed(int speed)
+        {
+            _speed = speed;
+        }
+        public void SetModel(string model)
+        {
+            _model = model;
+        }
+        public void SetColor(string color)
+        {
+            _color = color;
+        }
+        public void SetIsPassengerCar(bool isPassengerCar)
+        {
+            _isPassengerCar = isPassengerCar;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car[] cars = new Car[5];
+        }
+    }
+    partial class Car
+    {
+        public void Beep()
+        {
+            Console.WriteLine("Beep!");
+        }
+    }
+}
